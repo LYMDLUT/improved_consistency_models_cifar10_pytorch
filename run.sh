2e439c0bc463ff535a891b79552f7010285162b9
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2024-01-24 23:21:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd /home/liuyiming/final_code_v2/lkz_dae/cm
 accelerate launch --num_processes 8  --main_process_port=4066 ict.py
+accelerate launch --num_processes 8  --main_process_port=4066 ct.py
